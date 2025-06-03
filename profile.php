e9d146c55ae4acc8d33b43e1eede3c2ddef76ee2
syarif <syarifridwan@gmail.com>
2025-06-03 07:33:59 +0700

    pesan commit yang diinginkan

---
+++ profile.php
@@ -0,0 +1,49 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/1.png" alt="Muhammad Syarif Riduan" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NPM:</strong></td>
+                        <td align="left" width="50%">2310010459</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studi :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">MUHAMMAD SYARIF RIDUAN</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Muara Teweh, 21 September 2005</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">+62 858 2876 4829</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">syarifridwan433@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="https://wa.me/6281345617896" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/wa.svg" width="30" alt="whatsapp">
+                            </a>
+                            <a href="http://instagram.com/syariff.r_" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/ig.svg" width="30" alt="instagram">
+                            </a>
+                            <a href="https://x.com/17371s" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/x.svg" width="30" alt="x">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
